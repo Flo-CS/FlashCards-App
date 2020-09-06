63cfacccb067e21981360426acc5e52b61954eb9
DinoGraou <flodino03@gmail.com>
2020-09-06 16:55:20 +0200

    Finish AppHeader.jsx semantic UI design part

--- src/Components/Layout/AppHeader.jsx
+++ src/Components/Layout/AppHeader.jsx
@@ -6,16 +6,23 @@ export default function AppHeader() {
     return (<Menu>
         <Menu.Item>
             <Button icon>
-                <Icon name='bars' />
+                <Icon name='bars'/>
             </Button>
         </Menu.Item>
         <Menu.Item>
-            <Input/>
+            <Input icon={<Icon name="search"/>} placeholder="Search for card(s)..."/>
         </Menu.Item>
-            <Menu.Item position="right">
+        <Menu.Menu position="right">
+            <Menu.Item>
                 <Button icon>
-                <Icon name="settings"/>
+                    <Icon name="add"/>
                 </Button>
             </Menu.Item>
+            <Menu.Item>
+                <Button icon>
+                    <Icon name="settings"/>
+                </Button>
+            </Menu.Item>
+        </Menu.Menu>
     </Menu>)
 }
