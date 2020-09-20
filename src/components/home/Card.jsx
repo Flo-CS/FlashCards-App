107022c360123c8bed35cea8c23463a97a3beaad
DinoGraou <flodino03@gmail.com>
2020-09-20 15:50:31 +0200

    Fix bug where sidebar and topbar where behind the main view
    Update card component
    
    Reorder scss properties

--- src/components/home/Card.jsx
+++ src/components/home/Card.jsx
@@ -1,14 +1,27 @@
-import React from "react"
+import React, {useState} from "react"
 
 import "./Card.scss"
 
+import {IoMdTrash, IoMdCafe} from "react-icons/io";
+
 export default function Card({frontContent, backContent}) {
+    const [isBackShown, setIsBackShown] = useState(false)
+
+    function handleCardClick() {
+        setIsBackShown((isBackShown) => {
+            return !isBackShown
+        })
+    }
 
     return (<div className="card">
-        <div className="card__inner">
-            <div className="card__controls"></div>
-            <div className="card__front-content">{frontContent}</div>
-            <div className="card__back-content">{backContent}</div>
+        <div className="card__controls">
+            <button className="card__button"><IoMdCafe className="card__md-cafe-icon"/></button>
+            <button className="card__button"><IoMdTrash className="card__md-trash-icon"/></button>
+        </div>
+        <div className="card__inner" onClick={handleCardClick}>
+            {!isBackShown ?
+                <div className="card__front-content">{frontContent}</div> :
+                <div className="card__back-content">{backContent}</div>}
         </div>
     </div>)
 }
