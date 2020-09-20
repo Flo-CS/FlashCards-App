ff0e930885b9849c2a298d828104f057920ac5e9
DinoGraou <flodino03@gmail.com>
2020-09-20 15:02:16 +0200

    Finish not found page foundations

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from "react"
 
+import "./NotFound.scss"
+import {Link} from "react-router-dom";
+import {HOME} from "../constants/routes";
+
 export default function NotFound() {
     return (<main className="not-found">
-        404 Not Found
+        <h1 className="not-found__title">404 Not Found :/</h1>
+        <Link to={HOME}>Go to home</Link>
     </main>)
 }
