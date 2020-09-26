cbb23d5a0337d39fa495a7b66005bfdd24b6c10a
DinoGraou <flodino03@gmail.com>
2020-09-26 18:32:20 +0200

    Get folders from firestore to put in sidebar

--- src/components/home/SideBar.jsx
+++ src/components/home/SideBar.jsx
@@ -1,17 +1,42 @@
-import React from "react"
+import React, {useEffect} from "react"
 import ClassNames from "classnames"
 
 import "./SideBar.scss"
+import {firestoreGetUserData} from "../../utils/firestore";
+import {connect} from "react-redux";
+import {setFoldersAction} from "../../actions/foldersActions";
+import {foldersSelector} from "../../selectors/foldersSelectors";
 
-function SideBar({isOpened}) {
+
+function SideBar({folders, setFolders, isOpened}) {
+
+    useEffect(() => {
+        firestoreGetUserData().then((doc) => {
+            const data = doc.data()
+            const folders = data.folders
+            setFolders(folders)
+        })
+    }, [setFolders])
 
     const sideBarClasses = ClassNames({"side-bar": true, "side-bar--opened": isOpened})
 
     return (<div className={sideBarClasses}>
         <div className="side-bar__inner">
-
+            <p style={{wordBreak: "break-all"}}>{JSON.stringify(folders)}</p>
         </div>
     </div>)
 }
 
-export default React.memo(SideBar)
+function mapStateToProps(state) {
+    return {
+        folders: foldersSelector(state),
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        setFolders: (folders) => dispatch(setFoldersAction(folders))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SideBar)
