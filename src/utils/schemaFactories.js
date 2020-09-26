e57173be040154577e2cfed54c1c12455f439590
DinoGraou <flodino03@gmail.com>
2020-09-26 17:37:46 +0200

    Create folderSchemaFactory

--- src/utils/schemaFactories.js
+++ src/utils/schemaFactories.js
@@ -1,13 +1,21 @@
 import {nanoid} from "nanoid"
 
-export function cardSchemaFactory(frontContent = "", backContent = "", learningRate = "", folder = "/main") {
+export function cardSchemaFactory(frontContent = "", backContent = "", learningRate = "", folderPath = "/main") {
     return {
         id: nanoid(30),
         frontContent,
         backContent,
         learningRate,
-        folder,
+        folderPath,
         lastViewedDatetime: new Date().toISOString(),
         createdDatetime: new Date().toISOString(),
+    }
+}
+
+export function folderSchemaFactory(name, path) {
+    return {
+        id: nanoid(25),
+        name,
+        path
     }
 }
