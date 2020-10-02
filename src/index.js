30b2cdd297827ec85bc9e571deb157f2c968e597
DinoGraou <flodino03@gmail.com>
2020-10-02 15:23:37 +0200

    Add serviceWorker to make progressive web app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
+import * as serviceWorker from "./serviceWorker"
 
 import './index.scss';
 
@@ -14,3 +15,5 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+
+serviceWorker.register()
