a37f2badb6194039bfc9ad95797684785f5cafba
DinoGraou <flodino03@gmail.com>
2020-10-18 10:26:09 +0200

    Reimplement data saving to firestore but with a button in the TopBar to limit request to firebase

--- src/components/home/TopBar.jsx
+++ src/components/home/TopBar.jsx
@@ -4,7 +4,7 @@ import {connect} from "react-redux"
 
 import "./TopBar.scss"
 
-import {IoMdAdd, IoMdClose, IoMdLogOut, IoMdMenu, IoMdSearch} from "react-icons/io"
+import {IoMdAdd, IoMdClose, IoMdLogOut, IoMdMenu, IoMdSave, IoMdSearch} from "react-icons/io"
 import {fbAuthentication} from "../../utils/firebase";
 import CardModal from "../modal/cardModal/CardModal";
 import {cardSchemaFactory} from "../../utils/schemaFactories";
@@ -13,6 +13,7 @@ import {userLogoutAction} from "../../actions/rootActions";
 import ControlsGroup from "../controls/group/ControlsGroup";
 import {addCardAction} from "../../actions/cardsActions";
 import {selectedFolderSelector} from "../../selectors/foldersSelectors";
+import {saveDataToFirestore} from "../../utils/firestore";
 
 function TopBar({onToggleSideBarButtonClick, isSideBarOpened, selectedFolder, userLogout, addCard}) {
     const [isCardModalShown, setIsCardModalShown] = useState(false)
@@ -20,6 +21,10 @@ function TopBar({onToggleSideBarButtonClick, isSideBarOpened, selectedFolder, us
 
     function handleToggleSideBarButtonClick() {
         onToggleSideBarButtonClick()
+    }
+
+    function handleSaveDataButtonClick() {
+        saveDataToFirestore()
     }
 
     function handleLogoutButtonClick() {
@@ -71,6 +76,8 @@ function TopBar({onToggleSideBarButtonClick, isSideBarOpened, selectedFolder, us
                 <div className="TopBar__RightControls">
                     <ControlsGroup>
                         <button className="Button Button--Square" onClick={handleAddCardButtonClick}><IoMdAdd
+                            className="Button__Icon"/></button>
+                        <button className="Button Button--Square" onClick={handleSaveDataButtonClick}><IoMdSave
                             className="Button__Icon"/></button>
                         <button className="Button Button--Square" onClick={handleLogoutButtonClick}><IoMdLogOut
                             className="Button__Icon"/></button>
--- src/utils/firestore.js
+++ src/utils/firestore.js
@@ -1,4 +1,5 @@
 import {fbAuthentication, fbFirestore} from "./firebase";
+import store from "../store/store";
 
 export function firestoreGetUserData() {
     console.log("Read from firestore")
@@ -18,4 +19,16 @@ export function firestoreSetCards(cards) {
 export function firestoreSetFolders(folders) {
     console.log("Write to firestore")
     return fbFirestore.collection("users_data").doc(fbAuthentication.currentUser.uid).set({folders: folders}, {merge: true})
+}
+
+
+export function saveDataToFirestore() {
+    const appState = store.getState()
+
+    return firestoreSetUserData({
+            folders: appState.folders.foldersList,
+            cards: appState.cards.cardsList
+        }
+    )
+
 }
